454c500827656fe6c0805702305a3f698677df74
Liam T O'Toole <lotoole@uvm.edu>
2018-11-20 19:56:54 -0500

    updated schedule page to use acf image rather than built in one

--- wp-content/themes/winterfest/page-schedule.php
+++ wp-content/themes/winterfest/page-schedule.php
@@ -34,22 +34,21 @@ get_header();
   $image_class = 'right' == get_sub_field('alignment') ? '' : '';
   $content_class = 'right' == get_sub_field('alignment') ? 'order-last order-md-first' : '';
   $ID = strtolower(get_sub_field('hero_title'));
+  $day_image = get_sub_field('hero_image');
   ?>
 <section id="<?php echo preg_replace('/\s+/', '', $ID); ?>" class="day">
   <div class="container">
     <div class="row">
-      <!-- day -->
       <div class="<?php echo $content_class; ?> col-md-6">
         <h3><?php the_sub_field('title'); ?></h3>
         <?php the_sub_field('events'); ?>
       </div>
       <div class="<?php echo $image_class; ?> order-first col-md-6">
-        <div class="day-title">
+        <div class="day-title" style="background-image: url(<?php echo $day_image['url']; ?>)">
           <h3><?php the_sub_field('hero_title'); ?></h3>
           <span><?php the_sub_field('hero_date'); ?></span>
         </div>
       </div>
-      <!-- end of day -->
     </div>
   </div>
 </section>
