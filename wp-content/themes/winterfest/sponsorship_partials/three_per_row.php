5d3da8be83cae773e51bebfdbd1375ddd15c7e2c
Liam T O'Toole <lotoole@uvm.edu>
2018-12-21 11:30:10 -0500

    fixed sponsorship links

--- wp-content/themes/winterfest/sponsorship_partials/three_per_row.php
+++ wp-content/themes/winterfest/sponsorship_partials/three_per_row.php
@@ -3,8 +3,11 @@
 </div>
 <?php if( have_rows('sponsor') ): while( have_rows('sponsor') ) : the_row(); ?>
   <?php $image = get_sub_field('sponsor_image'); ?>
+  <?php $link = get_sub_field('sponsor_link');
+        $link_url = $link['url'];
+  ?>
   <div class="col-md-4">
-    <a href="<?php the_sub_field('link'); ?>">
+    <a href="<?php echo esc_url($link_url); ?>">
       <img src="<?php echo $image['url']; ?>" width="400px" height="400px" alt="">
       <!-- <img src="<?php //bloginfo('stylesheet_directory'); ?>/static/images/benandjerrys.png" width="400px" height="400px" alt=""> -->
     </a>
